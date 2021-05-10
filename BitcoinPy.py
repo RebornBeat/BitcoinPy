6c9e7cb916d3463bcc79e57f3ea2d5a0dbfc1d39
RebornBeat <christian.lfonts@gmail.com>
2021-05-10 13:13:17 -0400

    Add Connection Class
    Communicate with nodes and peers

--- BitcoinPy.py
+++ BitcoinPy.py
@@ -18,6 +18,9 @@ import hashlib
 from base58 import b58encode_check
 import binascii
 import json
+import socket
+import random
+import struct
 
 
 class Block(object):
@@ -191,18 +194,52 @@ class BlockChain(object):
 
         )
 
-    def check_for_chain():
-        if not os.path.isfile(Chain_Path):
-            return
-        try:
-            with open(Chain_Path, "r") as f:
-                imported_chain = json.load(f)
-                chain_length = len(imported_chain)
-                logger.info(f"Loading chain from disk with {chain_length} blocks")
-                for block in imported_chain:
-                    connect_block(block)
-        except Exception:
-            logger.exception('Failed to load chain, starting from genesis')
+
+    def check_for_corruption():
+        pass
+
+class Connection(object):
+    def __init__(self):
+        self.magic_value = 0xd9b4bef9
+        self.my_address = "127.0.0.1"
+        self.target_port = 8333
+
+    def create_sub_version(self):
+        sub_version = "/Satoshi:0.7.2/"
+        return b'\x0F' + sub_version.encode()
+
+    def encoded_network_address(self, ip_address):
+        print(ip_address)
+        network_address = struct.pack('>8s16sH', b'\x01', bytearray.fromhex("00000000000000000000ffff") + socket.inet_aton(ip_address), self.target_port)
+        return(network_address)
+
+    def create_message(self, command, payload):
+        checksum = hashlib.sha256(hashlib.sha256(payload).digest()).digest()[0:4]
+        return(struct.pack('L12sL4s', self.magic_value, command.encode(), len(payload), checksum) + payload)
+
+    def create_message_verack(self):
+        return bytearray.fromhex("f9beb4d976657261636b000000000000000000005df6e0e2")
+
+    def create_payload_version(self, seed):
+        version = 60002
+        services = 1
+        timestamp = int(time.time())
+        print(self.my_address)
+        addr_local = self.encoded_network_address(ip_address = self.my_address)
+        addr_peer = self.encoded_network_address(seed)
+        nonce = random.getrandbits(64)
+        start_height = 0
+        payload = struct.pack('<LQQ26s26sQ16sL', version, services, timestamp, addr_peer, addr_local, nonce, self.create_sub_version(), start_height)
+        return(payload)
+
+    def responce_format(self, command, request_data, response_data):
+        print("")
+        print("Command: " + command)
+        print("Request:")
+        print(request_data)
+        print("Response:")
+        print(response_data)
+        print("")
 
 
 def get_wallet():
@@ -230,12 +267,36 @@ def get_wallet():
 
     return signing_key, verifying_key, my_address
 
+def check_for_chain():
+    if not os.path.isfile(Chain_Path):
+        #logger.info('No chain found, starting from genesis')
+        print('No chain found, connecting to DNS Seeds')
+        connect_to_seeds()
+        return
+    try:
+        with open(Chain_Path, "r") as f:
+            imported_chain = json.load(f)
+            chain_length = len(imported_chain)
+            logger.info(f"Loading chain from disk with {chain_length} blocks")
+            for block in imported_chain:
+                check_for_corruption(block)
+    except Exception:
+        logger.exception('Failed to load chain, starting from genesis')
+
 
 if __name__ == '__main__':
 
     logging.basicConfig(filename='BitCoinPyLog.log', filemode='a', encoding='utf-8')
 
     logger = logging.getLogger(__name__)
+
+    DNS_SEEDS = [
+        "seed.bitcoinstats.com",
+        "seed.bitcoin.sipa.be",
+        "dnsseed.bluematt.me",
+        "dnsseed.bitcoin.dashjr.org",
+        "bitseed.xf2.org"
+    ]
 
     Wallet_Path = os.getcwd() + '\wallet.dat'
 
@@ -244,6 +305,8 @@ if __name__ == '__main__':
     Chain_Path = os.getcwd() + '\chain.dat'
 
     blockchain = BlockChain()
+
+    check_for_chain()
 
     print("GET READY MINING ABOUT TO START")
 
